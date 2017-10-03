4249a4a54221e55405ff5a041aeebbe9df70d3d5
ikjae1002 <ij374@nyu.edu>
2017-10-03 12:55:47 -0400

    part 1 completed

--- src/warmUp.js
+++ src/warmUp.js
@@ -1,1 +1,27 @@
 // warmUp.js
+
+const net = require('net');
+const HOST = '127.0.0.1';
+const PORT = 8080;
+
+function createResponse(status, body) {
+    return `HTTP/1.1 ${status} OK
+Content-Type: text/html
+
+${body}`;
+};
+
+const server = net.createServer((sock) => {
+    console.log(`got connection from ${sock.remoteAddress}:${sock.remotePort}`);
+
+    sock.write(createResponse(200,'<em>Hello</em> <strong>World</strong>')); 
+    // sock.on('data', (binaryData) => {
+    //     sock.write('echo: ' + s); 
+
+    //     // close the connection
+    //     sock.end();
+    // });
+    sock.end();
+});
+
+server.listen(PORT, HOST);
